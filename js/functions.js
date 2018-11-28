23db9817d5f6d52148e2fa52ba7998998be76783
ALaa EL Sayed <alaaelsayed662@yahoo.com>
2018-11-28 16:46:57 +0200

    Fahmny
    Web application

---
+++ js/functions.js
@@ -0,0 +1,193 @@
+/* JS OBFUSCATED */
+var _0x9baa=["\x6A\x51\x75\x65\x72\x79","\x75\x73\x65\x20\x73\x74\x72\x69\x63\x74","\x2E\x6D\x61\x69\x6E\x5F\x6E\x61\x76","\x23\x6D\x61\x69\x6E\x5F\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72","\x2E\x6C\x61\x79\x65\x72","\x23\x6D\x65\x6E\x75\x2D\x62\x75\x74\x74\x6F\x6E\x2D\x6D\x6F\x62\x69\x6C\x65","\x2E\x6D\x61\x69\x6E\x5F\x6E\x61\x76\x20\x2E\x6E\x61\x76\x2D\x74\x61\x62\x73\x20\x61","\x63\x6C\x69\x63\x6B","\x73\x68\x6F\x77","\x61\x64\x64\x43\x6C\x61\x73\x73","\x6C\x61\x79\x65\x72\x2D\x69\x73\x2D\x76\x69\x73\x69\x62\x6C\x65","\x6F\x6E","\x72\x65\x6D\x6F\x76\x65\x43\x6C\x61\x73\x73","\x61\x63\x74\x69\x76\x65","\x2E\x6D\x61\x69\x6E\x5F\x6E\x61\x76\x20\x2E\x6E\x61\x76\x2D\x74\x61\x62\x73\x20\x6C\x69\x2E\x61\x63\x74\x69\x76\x65","\x2E\x63\x6C\x6F\x73\x65\x5F\x69\x6E","\x68\x72\x65\x66","\x61\x74\x74\x72","\x74\x6F\x70","\x6F\x66\x66\x73\x65\x74","\x66\x61\x73\x74","\x61\x6E\x69\x6D\x61\x74\x65","\x2E\x77\x72\x61\x70\x70\x65\x72\x5F\x69\x6E","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x77\x69\x64\x74\x68","\x73\x6C\x69\x64\x65\x54\x6F\x67\x67\x6C\x65","\x74\x6F\x67\x67\x6C\x65\x43\x6C\x61\x73\x73","\x72\x65\x73\x69\x7A\x65","\x68\x69\x64\x65","\x62\x75\x74\x74\x6F\x6E\x2E\x62\x61\x63\x6B\x77\x61\x72\x64\x2C\x62\x75\x74\x74\x6F\x6E\x2E\x66\x6F\x72\x77\x61\x72\x64","\x69\x6E\x6E\x65\x72\x57\x69\x64\x74\x68","\x73\x6C\x6F\x77","\x74\x6F\x6F\x6C\x74\x69\x70","\x2E\x74\x6F\x6F\x6C\x74\x69\x70\x2D\x31","\x69\x63\x6F\x6E\x5F\x70\x6C\x75\x73\x5F\x61\x6C\x74\x32\x20\x69\x63\x6F\x6E\x5F\x6D\x69\x6E\x75\x73\x5F\x61\x6C\x74\x32","\x69\x2E\x69\x6E\x64\x69\x63\x61\x74\x6F\x72","\x66\x69\x6E\x64","\x2E\x70\x61\x6E\x65\x6C\x2D\x68\x65\x61\x64\x69\x6E\x67","\x70\x72\x65\x76","\x74\x61\x72\x67\x65\x74","\x68\x69\x64\x64\x65\x6E\x2E\x62\x73\x2E\x63\x6F\x6C\x6C\x61\x70\x73\x65\x20\x73\x68\x6F\x77\x6E\x2E\x62\x73\x2E\x63\x6F\x6C\x6C\x61\x70\x73\x65","\x2E\x70\x61\x6E\x65\x6C\x2D\x67\x72\x6F\x75\x70","\x69\x66\x72\x61\x6D\x65","\x6D\x61\x67\x6E\x69\x66\x69\x63\x50\x6F\x70\x75\x70","\x2E\x76\x69\x64\x65\x6F\x5F\x6D\x6F\x64\x61\x6C","\x61","\x69\x6D\x61\x67\x65","\x65\x61\x63\x68","\x2E\x6D\x61\x67\x6E\x69\x66\x69\x63\x2D\x67\x61\x6C\x6C\x65\x72\x79","\x66\x61\x64\x65\x4F\x75\x74","\x6F\x77\x6C\x43\x61\x72\x6F\x75\x73\x65\x6C","\x2E\x6F\x77\x6C\x2D\x63\x61\x72\x6F\x75\x73\x65\x6C","\x61\x63\x74\x69\x6F\x6E","\x71\x75\x6F\x74\x65\x5F\x73\x65\x6E\x64\x2E\x70\x68\x70","\x66\x6F\x72\x6D\x23\x77\x72\x61\x70\x70\x65\x64","\x3A\x72\x61\x64\x69\x6F","\x69\x73","\x3A\x63\x68\x65\x63\x6B\x62\x6F\x78","\x6E\x65\x78\x74","\x69\x6E\x73\x65\x72\x74\x42\x65\x66\x6F\x72\x65","\x69\x6E\x73\x65\x72\x74\x41\x66\x74\x65\x72","\x76\x61\x6C\x69\x64\x61\x74\x65","\x23\x77\x72\x61\x70\x70\x65\x64","\x2E\x73\x75\x62\x6D\x69\x74","\x6C\x65\x6E\x67\x74\x68","\x76\x61\x6C","\x69\x6E\x70\x75\x74\x23\x77\x65\x62\x73\x69\x74\x65","\x69\x73\x4D\x6F\x76\x69\x6E\x67\x46\x6F\x72\x77\x61\x72\x64","\x3A\x69\x6E\x70\x75\x74","\x73\x74\x65\x70","\x73\x74\x61\x74\x65","\x77\x69\x7A\x61\x72\x64","\x76\x61\x6C\x69\x64","\x23\x77\x69\x7A\x61\x72\x64\x5F\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72","\x70\x72\x6F\x67\x72\x65\x73\x73\x62\x61\x72","\x23\x70\x72\x6F\x67\x72\x65\x73\x73\x62\x61\x72","\x76\x61\x6C\x75\x65","\x70\x65\x72\x63\x65\x6E\x74\x43\x6F\x6D\x70\x6C\x65\x74\x65","\x28","\x73\x74\x65\x70\x73\x43\x6F\x6D\x70\x6C\x65\x74\x65","\x2F","\x73\x74\x65\x70\x73\x50\x6F\x73\x73\x69\x62\x6C\x65","\x29","\x74\x65\x78\x74","\x23\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x69\x63\x68\x65\x63\x6B\x62\x6F\x78\x5F\x73\x71\x75\x61\x72\x65\x2D\x79\x65\x6C\x6C\x6F\x77","\x69\x72\x61\x64\x69\x6F\x5F\x73\x71\x75\x61\x72\x65\x2D\x79\x65\x6C\x6C\x6F\x77","\x69\x43\x68\x65\x63\x6B","\x69\x6E\x70\x75\x74\x2E\x69\x63\x68\x65\x63\x6B"];(function(_0x794ax1){_0x9baa[1];var _0x794ax2=_0x794ax1(_0x9baa[2]);var _0x794ax3=_0x794ax1(_0x9baa[3]);var _0x794ax4=_0x794ax1(_0x9baa[4]);var _0x794ax5=_0x794ax1(_0x9baa[5]);var _0x794ax6=_0x794ax1(_0x9baa[6]);_0x794ax2[_0x9baa[11]](_0x9baa[7],function(){_0x794ax3[_0x9baa[9]](_0x9baa[8]);_0x794ax4[_0x9baa[9]](_0x9baa[10])});_0x794ax1(_0x9baa[15])[_0x9baa[11]](_0x9baa[7],function(){_0x794ax3[_0x9baa[12]](_0x9baa[8]);_0x794ax1(_0x9baa[14])[_0x9baa[12]](_0x9baa[13]);_0x794ax4[_0x9baa[12]](_0x9baa[10])});_0x794ax6[_0x9baa[11]](_0x9baa[7],function(_0x794ax7){var _0x794ax8=_0x794ax1(this)[_0x9baa[17]](_0x9baa[16]);_0x794ax1(_0x9baa[22])[_0x9baa[21]]({scrollTop:_0x794ax1(_0x794ax8)[_0x9baa[19]]()[_0x9baa[18]]},_0x9baa[20]);_0x794ax7[_0x9baa[23]]();if(_0x794ax1(window)[_0x9baa[24]]()<= 767){_0x794ax5[_0x9baa[12]](_0x9baa[13]);_0x794ax2[_0x9baa[25]](300)}});_0x794ax5[_0x9baa[11]](_0x9baa[7],function(){_0x794ax2[_0x9baa[25]](500);_0x794ax1(this)[_0x9baa[26]](_0x9baa[13])});_0x794ax1(window)[_0x9baa[11]](_0x9baa[27],function(){var _0x794ax9=_0x794ax1(window)[_0x9baa[24]]();if(_0x794ax9<= 767){_0x794ax2[_0x9baa[28]]()}else {_0x794ax2[_0x9baa[8]]()}});var _0x794axa=_0x794ax1(_0x9baa[29]);var _0x794axb=_0x794ax1(_0x9baa[22]);if(window[_0x9baa[30]]< 800){_0x794axa[_0x9baa[11]](_0x9baa[7],function(){_0x794axb[_0x9baa[21]]({scrollTop:500},_0x9baa[31]);return false})};if(window[_0x9baa[30]]< 600){_0x794axa[_0x9baa[11]](_0x9baa[7],function(){_0x794axb[_0x9baa[21]]({scrollTop:610},_0x9baa[31]);return false})};_0x794ax1(_0x9baa[33])[_0x9baa[32]]({html:true});function _0x794axc(_0x794ax7){_0x794ax1(_0x794ax7[_0x9baa[39]])[_0x9baa[38]](_0x9baa[37])[_0x9baa[36]](_0x9baa[35])[_0x9baa[26]](_0x9baa[34])}_0x794ax1(_0x9baa[41])[_0x9baa[11]](_0x9baa[40],_0x794axc);_0x794ax1(_0x9baa[44])[_0x9baa[43]]({type:_0x9baa[42]});_0x794ax1(_0x9baa[48])[_0x9baa[47]](function(){_0x794ax1(this)[_0x9baa[43]]({delegate:_0x9baa[45],type:_0x9baa[46],gallery:{enabled:true}})});_0x794ax1(_0x9baa[51])[_0x9baa[50]]({items:1,dots:false,loop:true,autoplay:true,autoHeight:true,autoplayTimeout:3500,animateOut:_0x9baa[49],responsive:{0:{items:1},600:{items:1},1000:{items:1}}});jQuery(function(_0x794ax1){_0x794ax1(_0x9baa[54])[_0x9baa[17]](_0x9baa[52],_0x9baa[53]);_0x794ax1(_0x9baa[73])[_0x9baa[71]]({stepsWrapper:_0x9baa[62],submit:_0x9baa[63],beforeSelect:function(_0x794axf,_0x794ax10){if(_0x794ax1(_0x9baa[66])[_0x9baa[65]]()[_0x9baa[64]]!= 0){return false};if(!_0x794ax10[_0x9baa[67]]){return true};var _0x794ax11=_0x794ax1(this)[_0x9baa[71]](_0x9baa[70])[_0x9baa[69]][_0x9baa[36]](_0x9baa[68]);return !_0x794ax11[_0x9baa[64]]||  !!_0x794ax11[_0x9baa[72]]()}})[_0x9baa[61]]({errorPlacement:function(_0x794axd,_0x794axe){if(_0x794axe[_0x9baa[56]](_0x9baa[55])|| _0x794axe[_0x9baa[56]](_0x9baa[57])){_0x794axd[_0x9baa[59]](_0x794axe[_0x9baa[58]]())}else {_0x794axd[_0x9baa[60]](_0x794axe)}}});_0x794ax1(_0x9baa[75])[_0x9baa[74]]();_0x794ax1(_0x9baa[73])[_0x9baa[71]]({afterSelect:function(_0x794axf,_0x794ax10){_0x794ax1(_0x9baa[75])[_0x9baa[74]](_0x9baa[76],_0x794ax10[_0x9baa[77]]);_0x794ax1(_0x9baa[84])[_0x9baa[83]](_0x9baa[78]+ _0x794ax10[_0x9baa[79]]+ _0x9baa[80]+ _0x794ax10[_0x9baa[81]]+ _0x9baa[82])}})});_0x794ax1(_0x9baa[88])[_0x9baa[87]]({checkboxClass:_0x9baa[85],radioClass:_0x9baa[86]})})(window[_0x9baa[0]])
+
+
+
+$("document").ready(function() {
+  $(".copy-link").click(function() {
+    $(".share-link").focus();
+    $(".share-link").select();
+    document.execCommand("copy");
+  });
+});
+
+
+
+
+
+
+
+/* Start  quiz */
+
+
+var quiz = {
+  //Array for questions
+  questions: [],
+
+  //Add questions as objects to array
+  addQuestion: function(question, correct, wrongOne, wrongTwo){
+    this.questions.push({
+      question: question,
+      correct: correct,
+      wrongOne: wrongOne,
+      wrongTwo: wrongTwo
+    });
+    //upadate number of questions each time you add one
+    view.displayNumberOfQuestions();
+  },
+
+  movingToNextQuestion: function(){
+    //find all next buttons and add event lisener to them
+    var nextButton = document.querySelectorAll(".nextButton");
+    for(i = 0; i < nextButton.length; i++){
+      nextButton[i].addEventListener("click", function(event){
+        //Find the element that was clicked
+        var elementClicked = event.target;
+        //If it was a next button then remove the is-active class from it parent
+        if(elementClicked.className === "nextButton"){
+          elementClicked.parentNode.classList.remove("is-active");
+          //If there isnt a next sibling then reshow the options to add questions and the info div
+          if(elementClicked.parentNode.nextElementSibling === null) {
+            var showAdd = document.querySelector(".addQuestions");
+            var showInfo = document.querySelector(".info");
+            showAdd.style.display = "block";
+            showInfo.style.display = "block";
+          } else {
+            //If there is a next siblng then add the is-active class to it
+            elementClicked.parentNode.nextElementSibling.classList.add("is-active");
+          }
+        }
+      });
+    };
+  }
+};
+
+var handlers = {
+  //This runs when you click add question button
+  addQuestion: function(){
+    //Get each of the inputs by id
+    var questionInput = document.getElementById("questionInput");
+    var correctInput = document.getElementById("correctInput");
+    var wrongOneInput = document.getElementById("wrongOneInput");
+    var wrongTwoInput = document.getElementById("wrongTwoInput");
+    //pass the values of the inputs into the addQuestion method in the quiz object which will add them to question array
+    quiz.addQuestion(questionInput.value, correctInput.value, wrongOneInput.value, wrongTwoInput.value);
+    //clear the inputs
+    questionInput.value = "";
+    correctInput.value = "";
+    wrongOneInput.value = "";
+    wrongTwoInput.value = "";
+  }
+}
+
+var view = {
+  //This runs when you click start quiz
+  displayQuestions: function(){
+    //Hide the options to add questions and the info
+    var hideAdd = document.querySelector(".addQuestions");
+    var hideInfo = document.querySelector(".info");
+    hideAdd.style.display = "none";
+    hideInfo.style.display = "none";
+    //Clear the quesitons wrapper
+    var questionsWrapper = document.querySelector(".questionsWrapper");
+    questionsWrapper.innerHTML = "";
+
+    //for each quesiton in array create elements neede and give classes
+    quiz.questions.forEach(function(question, index){
+      var questionDiv = document.createElement("div");
+      questionDiv.setAttribute("class", "questionDiv");
+      var nextButton = document.createElement("button");
+      nextButton.setAttribute("class", "nextButton");
+      var questionLi = document.createElement("li");
+      var correctLi = document.createElement("li");
+      correctLi.setAttribute("class", "correct");
+      var wrongOneLi = document.createElement("li");
+      wrongOneLi.setAttribute("class", "wrong");
+      var wrongTwoLi = document.createElement("li");
+      wrongTwoLi.setAttribute("class", "wrong");
+
+      //add each question div to the question wrapper
+      questionsWrapper.appendChild(questionDiv);
+
+      questionsWrapper.firstChild.classList.add("is-active");
+
+      //add the text to the inputs the values in the questions array
+      questionLi.textContent = question.question;
+      correctLi.textContent = question.correct;
+      wrongOneLi.textContent = question.wrongOne;
+      wrongTwoLi.textContent = question.wrongTwo;
+
+      //If its the last question the button should say finish if not it should say next
+      if (index === quiz.questions.length - 1){
+        nextButton.textContent = "Finish";
+      } else{
+        nextButton.textContent = "Next";
+      }
+
+      //Append elements to div
+      questionDiv.appendChild(questionLi);
+
+      //put the answers in a random order before apprending them so correct isnt always 1st
+      var array = [correctLi, wrongOneLi, wrongTwoLi];
+      array.sort(function(a, b){return 0.5 - Math.random()});
+      array.forEach(function(item){
+        questionDiv.appendChild(item);
+      });
+
+      questionDiv.appendChild(nextButton);
+
+      this.displayAnswersCorrect();
+      quiz.movingToNextQuestion();
+
+
+    }, this);
+  },
+
+  displayAnswersCorrect: function(){
+    var questionDiv = document.querySelectorAll(".questionDiv");
+    var correctAnswers = 0;
+    var answersCorrect = document.querySelector(".answersCorrect");
+    answersCorrect.textContent = " الأجابات الصحيحه: " + correctAnswers;
+
+    //add click event to each question div if the element clicked has class correct then add 1 to correctAnswers and change the color of element to green.
+    //Else change the color of element to red and find the elemtn with correct class and make it green
+    for (var i = 0; i < questionDiv.length; i++) {
+      questionDiv[i].onclick = function(event) {
+        event = event || window.event;
+        if(event.target.className === "correct"){
+          correctAnswers++;
+          answersCorrect.textContent = " الأجابات الصحيحه: " + correctAnswers;
+          event.target.style.color = "#2ecc71";
+        } else if(event.target.className === "wrong"){
+          event.target.style.color = "#e74c3c";
+          var itemChildren = event.target.parentNode.children;
+          for(i = 0; i < itemChildren.length; i++){
+            if(itemChildren[i].classList.contains("correct")){
+              itemChildren[i].style.color = "#2ecc71";
+            }
+          }
+        }
+        //Remove correct and wrong classes so the same question the score cant go up and colors cant chaneg
+        var itemChildren = event.target.parentNode.children;
+        for(i = 0; i < itemChildren.length; i++){
+          itemChildren[i].classList.remove("correct");
+          itemChildren[i].classList.remove("wrong");
+        }
+      }
+    }
+
+  },
+
+  //count objects in array to show how many questions added to screen
+  displayNumberOfQuestions: function(){
+    var numberLi = document.getElementById("NumberQuestionsInQuiz");
+    if(quiz.questions.length === 1) {
+      numberLi.textContent = "تم اضافه " + quiz.questions.length + "سؤال الي الكويز";
+    } else {
+      numberLi.textContent = "تم اضافه " + quiz.questions.length + " سؤال الي الكويز";
+    }
+  }
+}
+
+
+/* End   quiz */
